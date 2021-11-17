c242337f4eaab11bf742687b67e44b85eb8e5eb4
DioBruhhdo <91964841+DioBruhhdo@users.noreply.github.com>
2021-11-18 03:27:25 +0700

    Chỉnh sửa file ngôn ngữ strings.xml 1

--- app/src/main/java/com/example/crytocurrency_ltdt/fgment_setting.java
+++ app/src/main/java/com/example/crytocurrency_ltdt/fgment_setting.java
@@ -1,12 +1,23 @@
 package com.example.crytocurrency_ltdt;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
+
+import com.bumptech.glide.request.RequestOptions;
 
 public class fgment_setting extends PreferenceFragmentCompat {
+
+    public static final String My_share_Pref = "setting";
+    Context NCONTEXT;
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.setting, rootKey);
     }
+
+    SharedPreferences shP = PreferenceManager.getDefaultSharedPreferences(getContext());
+
 }
