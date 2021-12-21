95c8cc1fd3b3ebb3e79188c380d0bd347c5680c4
DioBruhhdo <91964841+DioBruhhdo@users.noreply.github.com>
2021-12-21 21:16:28 +0700

    comit trước khi pull 21/12/2021

--- app/src/main/java/com/example/crytocurrency_ltdt/activitive_screen_main.java
+++ app/src/main/java/com/example/crytocurrency_ltdt/activitive_screen_main.java
@@ -10,9 +10,12 @@ import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
+import androidx.preference.ListPreference;
 import androidx.preference.PreferenceManager;
 import androidx.viewpager.widget.ViewPager;
 
@@ -49,6 +52,29 @@ public class activitive_screen_main extends AppCompatActivity {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
         float f = Float.parseFloat(sharedPreferences.getString("textsize", "1.0f"));
          Util.adjustFontSize(this,f);
+         String dark_mode = sharedPreferences.getString("dark_mode", "MODE_NIGHT_FOLLOW_SYSTEM");
+            if (dark_mode.contains("MODE_NIGHT_NO")) {
+                Toast.makeText(this, R.string.dark_mode_light, Toast.LENGTH_SHORT).show();
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            }
+            else if (dark_mode.contains("MODE_NIGHT_YES")) {
+                Toast.makeText(this, R.string.dark_mode_dark, Toast.LENGTH_SHORT).show();
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            }
+            else if (dark_mode.contains("MODE_NIGHT_FOLLOW_SYSTEM")) {
+                Toast.makeText(this, R.string.dark_mode_system, Toast.LENGTH_SHORT).show();
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+            }
+            else if (dark_mode.contains("MODE_NIGHT_AUTO_TIME")) {
+                Toast.makeText(this, R.string.dark_mode_time, Toast.LENGTH_SHORT).show();
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_TIME);
+            }
+            else if (dark_mode.contains("MODE_NIGHT_AUTO_BATTERY")) {
+                Toast.makeText(this, R.string.dark_mode_pin, Toast.LENGTH_SHORT).show();
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY);
+            }
+
+
         setContentView(R.layout.activity_activitive_screen_main);
         List<Fragment> fragmentList = new ArrayList<>();
         fragmentList.add(new fgment_news());
